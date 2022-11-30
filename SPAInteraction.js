d7f8811beb8151d26fd42e95021838e0225aecd2
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2022-11-30 19:19:39 +0100

    Støtte for jQuery og baseURL

--- SPAInteraction.js
+++ SPAInteraction.js
@@ -6,9 +6,11 @@ var SPAInteraction = class {
      * @param interactionObjekt - Et objekt som representer interaction og har metoder: 
      *  showMessage(title, message, type), openDialog(title, msg, buttons) og hideLoading()
      */
-    constructor(interactionObjekt) {
-       this.baseURL = '/api/';
+    constructor(interactionObjekt, baseURL = '/api/') {
+       this.baseURL = baseURL;
        this.interactionObjekt = interactionObjekt;
+       // Sjekk om $ er definert, ellers bruk jQuery
+       this.$ = $ ? $ : jQuery;
     }
 
     // Interaction
@@ -17,22 +19,22 @@ var SPAInteraction = class {
     }
 
     removeElementFromDOM(el) {
-        $(el).fadeOut();
+        this.$(el).fadeOut();
     }
 
     removeElementFromDOMSlideUp(el) {
-        $(el).animate(
+        this.$(el).animate(
             {'min-height' : 0, 'max-height' : 0, height : 0, padding : 0, margin : 0}, 400, () => {
             this.removeElementFromDOM(el);
         });
     }
 
     appendHTML(el, html) {
-        $(el.append(html));
+        this.$(el.append(html));
     }
 
     fadeElementDOM(el) {
-        $(el).css('opacity', '.5');
+        this.$(el).css('opacity', '.5');
     }    
 
     showMessage(title, message, type) { // -1 -> 'error', 0 -> 'normal', 1 -> 'warning'
@@ -65,25 +67,25 @@ var SPAInteraction = class {
         
         // event is the event where the call has been triggered.
         // if the element is button then a loader will be added
-        var button = event ? ($(event.target).parent().parent().find('button')[0]) : null;
+        var button = event ? (this.$(event.target).parent().parent().find('button')[0]) : null;
         if(button) {
-            $(button).find('.spinner-border').detach();
-            $(button).append('<div class="spinner-border" role="status"><span class="sr-only"></span></div>');
+            this.$(button).find('.spinner-border').detach();
+            this.$(button).append('<div class="spinner-border" role="status"><span class="sr-only"></span></div>');
         }
 
         return new Promise((resolve, reject) => {  
             var _this = this;    
-            $.ajax({
+            this.$.ajax({
                 url: this.baseURL + url,
                 method: method,
                 data: method == 'GET' ? {} : data,
                 success: (res) => {
-                    $(button).find('.spinner-border').detach();
+                    this.$(button).find('.spinner-border').detach();
                     resolve(res);
                 }
             }).fail((res) => {
                 // The call has returned an error, remove the spinner
-                $(button).find('.spinner-border').detach();
+                this.$(button).find('.spinner-border').detach();
 
                 if(res.statusCode().status == 500) {
                     if(res.responseJSON.errorMessage) {
